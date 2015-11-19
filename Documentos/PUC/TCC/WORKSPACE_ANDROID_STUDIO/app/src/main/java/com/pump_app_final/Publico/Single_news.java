0a377e4b4a95b5207d083b64a0f96e95065f9143
Daniel Paranhos <danielcparanhos@gmail.com>
2015-11-18 22:25:52 -0200

    Commit da quarta feita 18/11

---
+++ Documentos/PUC/TCC/WORKSPACE_ANDROID_STUDIO/app/src/main/java/com/pump_app_final/Publico/Single_news.java
@@ -0,0 +1,18 @@
+package com.pump_app_final.Publico;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.pump_app_final.R;
+
+/**
+ * Created by daniel on 09/11/15.
+ */
+public class Single_news extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_news);
+    }
+}
